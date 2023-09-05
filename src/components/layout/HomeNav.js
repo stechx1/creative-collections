99da3a301ab27687c41b0881cdf1096f317c8711
techx1 <ss.uitech01@gmail.com>
2023-09-06 03:32:55 +0500

    remove target

--- src/components/layout/HomeNav.js
+++ src/components/layout/HomeNav.js
@@ -1,8 +1,8 @@
-import React, { Fragment, useState } from "react";
-import { Link, NavLink } from "react-router-dom";
-import { Row, Col, Button, Drawer } from "antd";
-import { MenuOutlined } from "@ant-design/icons";
-import { Colors } from "../../config/Colors";
+import React, { Fragment, useState } from 'react';
+import { Link, NavLink } from 'react-router-dom';
+import { Row, Col, Button, Drawer } from 'antd';
+import { MenuOutlined } from '@ant-design/icons';
+import { Colors } from '../../config/Colors';
 
 const HomeNav = ({ urlContainsNftMarketplace }) => {
   const [open, setOpen] = useState(false);
@@ -14,70 +14,72 @@ const HomeNav = ({ urlContainsNftMarketplace }) => {
   };
   return (
     <Fragment>
-      <Row style={{ width: "100%" }}>
+      <Row style={{ width: '100%' }}>
         <Col xs={20} sm={19} lg={4} md={20}>
-          <Link to="/">
+          <Link to='/'>
             <img
-              className="navlogo"
-              alt="logo"
-              height="80px"
-              src="/images/logo.png"
+              className='navlogo'
+              alt='logo'
+              height='80px'
+              src='/images/logo.png'
             />
           </Link>
         </Col>
-        <Col className="hidemob" xs={8} sm={5} lg={2} md={3}>
+        <Col className='hidemob' xs={8} sm={5} lg={2} md={3}>
           <a
             style={{
-              color: urlContainsNftMarketplace ? Colors?.main : "white",
+              color: urlContainsNftMarketplace ? Colors?.main : 'white',
             }}
-            href="https://www.c8cstudio.com/"
-            target="_blank"
+            href='https://www.c8cstudio.com/'
+            target='_blank'
+            rel='noreferrer'
           >
             Home
           </a>
         </Col>
-        <Col className="hidemob" xs={8} sm={5} lg={4} md={3}>
+        <Col className='hidemob' xs={8} sm={5} lg={4} md={3}>
           <a
             style={{
-              color: urlContainsNftMarketplace ? Colors?.main : "white",
+              color: urlContainsNftMarketplace ? Colors?.main : 'white',
             }}
-            href="https://c8cstudio-vr.com/c8cstudios"
-            target="_blank"
+            href='https://c8cstudio-vr.com/c8cstudios'
+            target='_blank'
+            rel='noreferrer'
           >
             C8C VR (Desktop)
           </a>
         </Col>
-        <Col className="hidemob" xs={8} sm={5} lg={4} md={3}>
+        <Col className='hidemob' xs={8} sm={5} lg={4} md={3}>
           <a
             style={{
-              color: urlContainsNftMarketplace ? Colors?.main : "white",
+              color: urlContainsNftMarketplace ? Colors?.main : 'white',
             }}
-            href="https://www.c8cstudio.com/product-page/cre8tive-collective-vr"
-            target="_blank"
+            href='https://www.c8cstudio.com/product-page/cre8tive-collective-vr'
+            target='_blank'
+            rel='noreferrer'
           >
             C8C VR (Meta Quest 2)
           </a>
         </Col>
 
-        <Col className="hidemob" xs={8} sm={5} lg={4} md={3}>
+        <Col className='hidemob' xs={8} sm={5} lg={4} md={3}>
           <Link
             style={{
-              color: urlContainsNftMarketplace ? Colors?.main : "white",
+              color: urlContainsNftMarketplace ? Colors?.main : 'white',
             }}
-            to="/nftmarketplace"
+            to='/nftmarketplace'
           >
             Virtual Collections
           </Link>
         </Col>
-        <Col className="hidemob" xs={8} sm={4} lg={2} md={3}>
+        <Col className='hidemob' xs={8} sm={4} lg={2} md={3}>
           <a
-            className="navlink"
+            className='navlink'
             style={{
-              color: urlContainsNftMarketplace ? Colors?.main : "white",
-              fontWeight: "500",
+              color: urlContainsNftMarketplace ? Colors?.main : 'white',
+              fontWeight: '500',
             }}
-            href="https://www.c8cstudio.com/plans-pricing"
-            target="_blank"
+            href='https://www.c8cstudio.com/plans-pricing'
           >
             Sign Up
           </a>
@@ -95,113 +97,119 @@ const HomeNav = ({ urlContainsNftMarketplace }) => {
             <MenuOutlined />
           </Button> */}
         </Col>
-        <Col className="hidemob" xs={8} sm={4} lg={2} md={3}>
+        <Col className='hidemob' xs={8} sm={4} lg={2} md={3}>
           <a
-            className="navlink"
-            href="https://www.c8cstudio.com"
-            target="_blank"
+            className='navlink'
+            href='https://www.c8cstudio.com'
+            target='_blank'
+            rel='noreferrer'
           >
             <Button
               style={{
-                height: "34px",
-                borderRadius: "6px",
-                width: "110px",
+                height: '34px',
+                borderRadius: '6px',
+                width: '110px',
                 backgroundColor: Colors.main,
                 borderColor: Colors.main,
               }}
-              type="primary"
+              type='primary'
             >
               Log In
             </Button>
           </a>
         </Col>
 
-        <Col className="hideweb" xs={4} sm={4} lg={2} md={3}>
+        <Col className='hideweb' xs={4} sm={4} lg={2} md={3}>
           <Button
             style={{
-              height: "34px",
-              borderRadius: "6px",
+              height: '34px',
+              borderRadius: '6px',
               backgroundColor: Colors.main,
               borderColor: Colors.main,
             }}
-            type="primary"
+            type='primary'
             onClick={showDrawer}
           >
             <MenuOutlined />
           </Button>
         </Col>
       </Row>
-      <Drawer title="C8cstudio" placement="right" onClose={onClose} open={open}>
+      <Drawer title='C8cstudio' placement='right' onClose={onClose} open={open}>
         <div>
           <a
-            className="navlink"
-            style={{ color: "black", fontWeight: "500" }}
-            href="https://www.c8cstudio.com"
-            target="_blank"
+            className='navlink'
+            style={{ color: 'black', fontWeight: '500' }}
+            href='https://www.c8cstudio.com'
+            target='_blank'
+            rel='noreferrer'
           >
             Home
-          </a>{" "}
+          </a>{' '}
         </div>
         <br />
         <a
-          className="navlink"
-          style={{ color: "black", fontWeight: "500" }}
-          href="https://www.c8cstudio.com/plans-pricing"
-          target="_blank"
+          className='navlink'
+          style={{ color: 'black', fontWeight: '500' }}
+          href='https://www.c8cstudio.com/plans-pricing'
+          target='_blank'
+          rel='noreferrer'
         >
           Signup
         </a>
         <br />
         <a
-          className="navlink"
-          href="https://www.c8cstudio.com"
-          target="_blank"
+          className='navlink'
+          href='https://www.c8cstudio.com'
+          target='_blank'
           style={{
-            color: "black",
-            fontWeight: "500",
-            display: "inline-block",
-            marginTop: "20px",
+            color: 'black',
+            fontWeight: '500',
+            display: 'inline-block',
+            marginTop: '20px',
           }}
+          rel='noreferrer'
         >
           Login
         </a>
         <br />
         <a
-          className="navlink"
-          href="https://www.c8cstudio.com/product-page/cre8tive-collective-vr"
-          target="_blank"
+          className='navlink'
+          href='https://www.c8cstudio.com/product-page/cre8tive-collective-vr'
+          target='_blank'
           style={{
-            color: "black",
-            fontWeight: "500",
-            display: "inline-block",
-            marginTop: "20px",
+            color: 'black',
+            fontWeight: '500',
+            display: 'inline-block',
+            marginTop: '20px',
           }}
+          rel='noreferrer'
         >
           C8C VR (Meta Quest 2)
         </a>
         <br />
         <a
-          className="navlink"
-          href="https://c8cstudio-vr.com/c8cstudios"
-          target="_blank"
+          className='navlink'
+          href='https://c8cstudio-vr.com/c8cstudios'
+          target='_blank'
           style={{
-            color: "black",
-            fontWeight: "500",
-            display: "inline-block",
-            marginTop: "20px",
+            color: 'black',
+            fontWeight: '500',
+            display: 'inline-block',
+            marginTop: '20px',
           }}
+          rel='noreferrer'
         >
           C8C VR (Desktop)
         </a>
         <br />
         <Link
           style={{
-            color: "black",
-            fontWeight: "500",
-            display: "inline-block",
-            marginTop: "20px",
+            color: 'black',
+            fontWeight: '500',
+            display: 'inline-block',
+            marginTop: '20px',
           }}
-          to="/nftmarketplace"
+          to='/nftmarketplace'
         >
           Virtual Collections
         </Link>
